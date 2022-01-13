f7b773f83784cf0e532e60aa7b0905b58057c3d8
emalderete <nicolas.alderete95@live.com>
2022-01-13 14:11:59 -0300

    arreglos en el fondo y cuerpo del cv
    Agregado nuevo color de fondo y eliminado el botón de "Habilidades" del cuerpo del proyecto.
    La sección de "Habilidades" irá incluida junto a la información personal.

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// El proyecto de un curriculum vitae no requiere intervención de JavaScript dado que se trata de una página
+// donde irá maquetada información para mostrar.
+// De todas formas este archivo existe en caso de que se considere necesario incluir lago de JS al proyecto
+// según las funciones que pueda llegar a tener dicho proyecto.
