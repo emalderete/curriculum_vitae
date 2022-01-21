8a0d3a248b8495bbccb8a9b22c5a63454ed8a51e
emalderete <nicolas.alderete95@live.com>
2022-01-21 14:32:11 -0300

    Botón de menú desplegable funcional
    Se realizan los arreglos necesarios sobre el código del botón del menú desplegable para hacerlo completamente funcional, ahora al tocar el botón hamburguesa se pliega y despliega el menú lateral.
    
    Próximamente se trabajará en darle más animación al menú en cuestión

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 const info = document.querySelector(".basics");
 const infoTrigger = document.querySelector(".basics-button");
+const infoTriggerClose = document.querySelector(".basics-button-close");
 
-infoTrigger.addEventListener('click', () => {
+infoTrigger.addEventListener('click', () => { shot() });
+infoTriggerClose.addEventListener('click', () => { shotClose() });
+
+
+function shot() {
     info.style.display = "block";
-});
+}
+
+function shotClose() {
+    info.style.display = "none";
+}
