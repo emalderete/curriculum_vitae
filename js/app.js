a0a5f66cd1c96fa043111f3c04df3fdab9f003ab
emalderete <nicolas.alderete95@live.com>
2022-01-21 00:16:31 -0300

    Nuevas funciones (leer desc.)
    Se realizaron grandes modificaciones en el proyecto:
    • Eliminado el sistema de columnas mediante clases (incluidas en bootstrap).
    • Se modifican los tamaños de los contenedores de información y principal.
    • se ajustan los márgenes contenedor de información.
    • se agrega la lógica necesaria para agregar una nueva característica: Ahora el contenedor de información será plegado en un menú lateral cuando la resolución de pantalla sea inferior a 992px.
    
    Se requieren muchas modificaciones sobre la lógica del menú desplegable dado que aún no está completa al 100%

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-// El proyecto de un curriculum vitae no requiere intervención de JavaScript dado que se trata de una página
-// donde irá maquetada información para mostrar.
-// De todas formas este archivo existe en caso de que se considere necesario incluir algo de JS al proyecto
-// según las funciones que pueda llegar a tener dicho proyecto.
+const info = document.querySelector(".basics");
+const infoTrigger = document.querySelector(".basics-button");
+
+infoTrigger.addEventListener('click', () => {
+    info.style.display = "block";
+});
