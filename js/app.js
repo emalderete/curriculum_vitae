6dca8279c94edc27759986abf1a39ab38f65eb28
emalderete <nicolas.alderete95@live.com>
2022-01-22 19:18:08 -0300

    Agregado diseño de overlay
    Se agrega diseño de overlay oscuro al desplegar el menú lateral y retirarlo al plegar dicho menú nuevamente.
    Se realizan ajustes sobre la resolución general de la página en su altura.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
 const info = document.querySelector(".basics");
 const infoTrigger = document.querySelector(".basics-button");
 const infoTriggerClose = document.querySelector(".basics-button-close");
+const mainSquare = document.querySelector("#overlay");
 
 infoTrigger.addEventListener('click', () => { shot() });
 infoTriggerClose.addEventListener('click', () => { shotClose() });
 
 
 function shot() {
-    info.style.display = "block";
+    info.style.cssText = "width:80%;transition:650ms";
+    mainSquare.classList.toggle("darkOverlay");
 }
 
 function shotClose() {
-    info.style.display = "none";
+    info.style.cssText = "margin-left:-80%;transition:350ms";
+    mainSquare.classList.remove("darkOverlay");
+}
+
+function reloadPageFrom() {
+    if (window.matchMedia("min-width: 992px")) {
+        info.style.marginLeft = "0%";
+    }
 }
